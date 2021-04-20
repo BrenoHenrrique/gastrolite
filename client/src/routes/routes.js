0d36211192065b424b17d28e3bae0350c6933a55
Breno Henrique <34743257+BrenoHenrrique@users.noreply.github.com>
2021-04-19 21:03:22 -0300

    ajustes

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route, Router} from "react-router-dom";
+import {Switch, Route} from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 
 import Login from "../view/login";
@@ -13,17 +13,15 @@ import Funcionarios from "../view/funcionarios";
 
 export default function Routes() {
     return (
-        <Router history={""}>
-            <Switch>
-                <Route exact path={"/login"} component={Login}/>
-                <PrivateRoute exact path={"/"} component={Dashboard}/>
-                <PrivateRoute exact path={"/cardapio"} component={Cardapio}/>
-                <PrivateRoute exact path={"/entregas"} component={Entregas}/>
-                <PrivateRoute exact path={"/vendaRapida"} component={VendaRapida}/>
-                <PrivateRoute exact path={"/clientes"} component={Clientes}/>
-                <PrivateRoute exact path={"/locais"} component={Locais}/>
-                <PrivateRoute exact path={"/funcionarios"} component={Funcionarios}/>
-            </Switch>
-        </Router>
+        <Switch>
+            <Route exact path={"/login"} component={Login}/>
+            <PrivateRoute exact path={"/"} component={Dashboard}/>
+            <PrivateRoute exact path={"/cardapio"} component={Cardapio}/>
+            <PrivateRoute exact path={"/entregas"} component={Entregas}/>
+            <PrivateRoute exact path={"/vendaRapida"} component={VendaRapida}/>
+            <PrivateRoute exact path={"/clientes"} component={Clientes}/>
+            <PrivateRoute exact path={"/locais"} component={Locais}/>
+            <PrivateRoute exact path={"/funcionarios"} component={Funcionarios}/>
+        </Switch>
     );
 }
